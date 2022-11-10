b1585b2b1ca6276e1941c27319115d43f13eb6b3
Zachary Braden <103508025+kobrakon@users.noreply.github.com>
2022-11-10 12:13:17 -0500

    forgor to upload

--- RONML-UI/Form1.Designer.cs
+++ RONML-UI/Form1.Designer.cs
@@ -1,4 +1,4 @@
-﻿namespace RONML_UI
+namespace RONML_UI
 {
     partial class Form1
     {
@@ -40,6 +40,8 @@
             this.checkBox4 = new System.Windows.Forms.CheckBox();
             this.checkBox5 = new System.Windows.Forms.CheckBox();
             this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // button1
@@ -48,10 +50,9 @@
             this.button1.Dock = System.Windows.Forms.DockStyle.Bottom;
             this.button1.Font = new System.Drawing.Font("Cascadia Mono SemiBold", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
             this.button1.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
-            this.button1.Location = new System.Drawing.Point(0, 468);
-            this.button1.Margin = new System.Windows.Forms.Padding(2);
+            this.button1.Location = new System.Drawing.Point(0, 686);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(682, 82);
+            this.button1.Size = new System.Drawing.Size(1046, 137);
             this.button1.TabIndex = 0;
             this.button1.Text = "Start Modded";
             this.button1.UseVisualStyleBackColor = false;
@@ -63,10 +64,9 @@
             this.button2.Dock = System.Windows.Forms.DockStyle.Bottom;
             this.button2.Font = new System.Drawing.Font("Cascadia Mono SemiBold", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
             this.button2.ForeColor = System.Drawing.SystemColors.ButtonFace;
-            this.button2.Location = new System.Drawing.Point(0, 386);
-            this.button2.Margin = new System.Windows.Forms.Padding(2);
+            this.button2.Location = new System.Drawing.Point(0, 549);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(682, 82);
+            this.button2.Size = new System.Drawing.Size(1046, 137);
             this.button2.TabIndex = 1;
             this.button2.Text = "Start Vanilla";
             this.button2.UseVisualStyleBackColor = false;
@@ -81,10 +81,9 @@
             this.checkBox1.Dock = System.Windows.Forms.DockStyle.Bottom;
             this.checkBox1.Font = new System.Drawing.Font("Cascadia Mono SemiBold", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
             this.checkBox1.ForeColor = System.Drawing.SystemColors.ButtonFace;
-            this.checkBox1.Location = new System.Drawing.Point(0, 361);
-            this.checkBox1.Margin = new System.Windows.Forms.Padding(2);
+            this.checkBox1.Location = new System.Drawing.Point(0, 513);
             this.checkBox1.Name = "checkBox1";
-            this.checkBox1.Size = new System.Drawing.Size(682, 25);
+            this.checkBox1.Size = new System.Drawing.Size(1046, 36);
             this.checkBox1.TabIndex = 2;
             this.checkBox1.Text = "Load VO";
             this.checkBox1.UseVisualStyleBackColor = false;
@@ -99,10 +98,9 @@
             this.checkBox2.Dock = System.Windows.Forms.DockStyle.Bottom;
             this.checkBox2.Font = new System.Drawing.Font("Cascadia Mono SemiBold", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
             this.checkBox2.ForeColor = System.Drawing.SystemColors.ButtonFace;
-            this.checkBox2.Location = new System.Drawing.Point(0, 336);
-            this.checkBox2.Margin = new System.Windows.Forms.Padding(2);
+            this.checkBox2.Location = new System.Drawing.Point(0, 477);
             this.checkBox2.Name = "checkBox2";
-            this.checkBox2.Size = new System.Drawing.Size(682, 25);
+            this.checkBox2.Size = new System.Drawing.Size(1046, 36);
             this.checkBox2.TabIndex = 3;
             this.checkBox2.Text = "Load PAK";
             this.checkBox2.UseVisualStyleBackColor = false;
@@ -117,10 +115,9 @@
             this.checkBox3.Dock = System.Windows.Forms.DockStyle.Bottom;
             this.checkBox3.Font = new System.Drawing.Font("Cascadia Mono SemiBold", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
             this.checkBox3.ForeColor = System.Drawing.SystemColors.ButtonFace;
-            this.checkBox3.Location = new System.Drawing.Point(0, 311);
-            this.checkBox3.Margin = new System.Windows.Forms.Padding(2);
+            this.checkBox3.Location = new System.Drawing.Point(0, 441);
             this.checkBox3.Name = "checkBox3";
-            this.checkBox3.Size = new System.Drawing.Size(682, 25);
+            this.checkBox3.Size = new System.Drawing.Size(1046, 36);
             this.checkBox3.TabIndex = 4;
             this.checkBox3.Text = "Load FMOD";
             this.checkBox3.UseVisualStyleBackColor = false;
@@ -134,12 +131,10 @@
             this.label1.Font = new System.Drawing.Font("Cascadia Mono SemiBold", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
             this.label1.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
             this.label1.Location = new System.Drawing.Point(0, 0);
-            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(91, 21);
+            this.label1.Size = new System.Drawing.Size(140, 32);
             this.label1.TabIndex = 5;
             this.label1.Text = "Game Path";
-            this.label1.Click += new System.EventHandler(this.label1_Click);
             // 
             // richTextBox1
             // 
@@ -147,10 +142,9 @@
             this.richTextBox1.Dock = System.Windows.Forms.DockStyle.Top;
             this.richTextBox1.Font = new System.Drawing.Font("Cascadia Mono SemiBold", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
             this.richTextBox1.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
-            this.richTextBox1.Location = new System.Drawing.Point(0, 21);
-            this.richTextBox1.Margin = new System.Windows.Forms.Padding(2);
+            this.richTextBox1.Location = new System.Drawing.Point(0, 32);
             this.richTextBox1.Name = "richTextBox1";
-            this.richTextBox1.Size = new System.Drawing.Size(682, 21);
+            this.richTextBox1.Size = new System.Drawing.Size(1046, 32);
             this.richTextBox1.TabIndex = 6;
             this.richTextBox1.Text = "No path!";
             this.richTextBox1.TextChanged += new System.EventHandler(this.richTextBox1_TextChanged);
@@ -162,13 +156,11 @@
             this.label2.Dock = System.Windows.Forms.DockStyle.Top;
             this.label2.Font = new System.Drawing.Font("Cascadia Mono", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
             this.label2.ForeColor = System.Drawing.SystemColors.ButtonFace;
-            this.label2.Location = new System.Drawing.Point(0, 42);
-            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label2.Location = new System.Drawing.Point(0, 64);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(475, 63);
+            this.label2.Size = new System.Drawing.Size(712, 95);
             this.label2.TabIndex = 7;
             this.label2.Text = "RON Mod Launcher";
-            this.label2.Click += new System.EventHandler(this.label2_Click);
             // 
             // label3
             // 
@@ -177,14 +169,12 @@
             this.label3.Dock = System.Windows.Forms.DockStyle.Top;
             this.label3.Font = new System.Drawing.Font("Cascadia Mono SemiBold", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
             this.label3.ForeColor = System.Drawing.SystemColors.ButtonFace;
-            this.label3.Location = new System.Drawing.Point(0, 105);
-            this.label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
-            this.label3.MaximumSize = new System.Drawing.Size(595, 0);
+            this.label3.Location = new System.Drawing.Point(0, 159);
+            this.label3.MaximumSize = new System.Drawing.Size(900, 0);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(100, 21);
+            this.label3.Size = new System.Drawing.Size(154, 32);
             this.label3.TabIndex = 8;
             this.label3.Text = "randomText";
-            this.label3.Click += new System.EventHandler(this.label3_Click);
             // 
             // checkBox4
             // 
@@ -195,10 +185,9 @@
             this.checkBox4.Dock = System.Windows.Forms.DockStyle.Bottom;
             this.checkBox4.Font = new System.Drawing.Font("Cascadia Mono SemiBold", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
             this.checkBox4.ForeColor = System.Drawing.SystemColors.ButtonFace;
-            this.checkBox4.Location = new System.Drawing.Point(0, 286);
-            this.checkBox4.Margin = new System.Windows.Forms.Padding(2);
+            this.checkBox4.Location = new System.Drawing.Point(0, 405);
             this.checkBox4.Name = "checkBox4";
-            this.checkBox4.Size = new System.Drawing.Size(682, 25);
+            this.checkBox4.Size = new System.Drawing.Size(1046, 36);
             this.checkBox4.TabIndex = 9;
             this.checkBox4.Text = "Start In DX12";
             this.checkBox4.UseVisualStyleBackColor = false;
@@ -213,10 +202,9 @@
             this.checkBox5.Dock = System.Windows.Forms.DockStyle.Bottom;
             this.checkBox5.Font = new System.Drawing.Font("Cascadia Mono SemiBold", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
             this.checkBox5.ForeColor = System.Drawing.SystemColors.ButtonFace;
-            this.checkBox5.Location = new System.Drawing.Point(0, 261);
-            this.checkBox5.Margin = new System.Windows.Forms.Padding(2);
+            this.checkBox5.Location = new System.Drawing.Point(0, 369);
             this.checkBox5.Name = "checkBox5";
-            this.checkBox5.Size = new System.Drawing.Size(682, 25);
+            this.checkBox5.Size = new System.Drawing.Size(1046, 36);
             this.checkBox5.TabIndex = 10;
             this.checkBox5.Text = "Override All VO";
             this.checkBox5.UseVisualStyleBackColor = false;
@@ -229,20 +217,53 @@
             this.button3.Dock = System.Windows.Forms.DockStyle.Bottom;
             this.button3.Font = new System.Drawing.Font("Cascadia Mono SemiBold", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
             this.button3.ForeColor = System.Drawing.SystemColors.ButtonFace;
-            this.button3.Location = new System.Drawing.Point(0, 550);
+            this.button3.Location = new System.Drawing.Point(0, 823);
+            this.button3.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(682, 28);
+            this.button3.Size = new System.Drawing.Size(1046, 47);
             this.button3.TabIndex = 11;
             this.button3.Text = "Select Game Folder";
             this.button3.UseVisualStyleBackColor = false;
             this.button3.Click += new System.EventHandler(this.button3_Click);
             // 
+            // button4
+            // 
+            this.button4.AutoSize = true;
+            this.button4.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.button4.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.button4.Font = new System.Drawing.Font("Cascadia Mono SemiBold", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.button4.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.button4.Location = new System.Drawing.Point(0, 870);
+            this.button4.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(1046, 44);
+            this.button4.TabIndex = 12;
+            this.button4.Text = "Backup Lossable Files";
+            this.button4.UseVisualStyleBackColor = false;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.AutoSize = true;
+            this.button5.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.button5.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.button5.Font = new System.Drawing.Font("Cascadia Mono SemiBold", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.button5.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.button5.Location = new System.Drawing.Point(0, 914);
+            this.button5.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(1046, 44);
+            this.button5.TabIndex = 13;
+            this.button5.Text = "Import Backup";
+            this.button5.UseVisualStyleBackColor = false;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 25F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
-            this.ClientSize = new System.Drawing.Size(682, 578);
+            this.ClientSize = new System.Drawing.Size(1046, 958);
             this.Controls.Add(this.checkBox5);
             this.Controls.Add(this.checkBox4);
             this.Controls.Add(this.label3);
@@ -255,7 +276,8 @@
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.button3);
-            this.Margin = new System.Windows.Forms.Padding(2);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button5);
             this.Name = "Form1";
             this.Text = "RON Mod Launcher";
             this.ResumeLayout(false);
@@ -277,5 +299,7 @@
         internal Label label3;
         internal CheckBox checkBox5;
         internal Button button3;
+        internal Button button4;
+        internal Button button5;
     }
 }
