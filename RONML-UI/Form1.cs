a6f8b0f7f74ac179596ed748daf164f55b117ee8
Zachary Braden <103508025+kobrakon@users.noreply.github.com>
2022-11-10 12:13:50 -0500

    forgor to upload

--- RONML-UI/Form1.cs
+++ RONML-UI/Form1.cs
@@ -3,63 +3,7 @@ namespace RONML_UI
 {
     public partial class Form1 : Form
     {
-        public Form1()
-        {
-            InitializeComponent();
-            Random random = new();
-            label3.Text = RandomText[random.Next(RandomText.Count)];
-            Application.ApplicationExit += new EventHandler(ExitEventHandler);
-            richTextBox1.Text = IOScripts.GamePath;
-            if (!Directory.Exists($@"{IOScripts.Me}\modcontent"))
-            {
-                Directory.CreateDirectory(IOScripts.Modvo);
-                Directory.CreateDirectory(IOScripts.Paks);
-                Directory.CreateDirectory(IOScripts.Modbanks);
-                Directory.CreateDirectory(IOScripts.Gamevotemp);
-                Directory.CreateDirectory(IOScripts.Gamebanktemp);
-            }
-        }
-
-        async void button1_Click(object sender, EventArgs e)
-        {
-            label3.Text = "Starting file transfers";
-            await Task.Run(() => IOScripts.SwitchIO());
-        }
-
-        void checkBox1_CheckedChanged(object sender, EventArgs e)
-        {
-            IOScripts.LoadVo = !IOScripts.LoadVo;
-        }
-
-        void checkBox3_CheckedChanged(object sender, EventArgs e)
-        {
-            IOScripts.LoadFmod = !IOScripts.LoadFmod;
-        }
-
-        void checkBox2_CheckedChanged(object sender, EventArgs e)
-        {
-            IOScripts.LoadPak = !IOScripts.LoadPak;
-        }
-
-        void label1_Click(object sender, EventArgs e)
-        {
-            return;
-        }
-
-        void richTextBox1_TextChanged(object sender, EventArgs e)
-        {
-            IOScripts.GamePath = richTextBox1.Text;
-        }
-
-        void label3_Click(object sender, EventArgs e)
-        {
-            return;
-        }
-
-        void checkBox4_CheckedChanged(object sender, EventArgs e)
-        {
-            IOScripts.Loaddx12 = !IOScripts.Loaddx12;
-        }
+        IOScripts IO = new();
 
         readonly List<string> RandomText = new()
         {
@@ -81,27 +25,46 @@ namespace RONML_UI
             "Five Guys ARs and Nines",
             "I wanna use the MP5/10mm, but the bolt release thing is cursed",
             "Remember: Evil is never dead enough",
-            "All fun and games till the host dies and restarts the game"
+            "All fun and games till the host dies and restarts the game",
+            "Do you feel like a hero yet?",
+            "That moment when a suspect shoots you 15 times but you still get penalized for unauthorized use of force",
+            "That moment when a suspect with a tiny .38 penetrates your full suit of AR500 steel armor and kills you",
+            "You should always have more firepower than the suspects, go ahead and bring the grenade launcher",
+            "Frag grenades when????? Void pls",
+            "The quicker you shoot, the quicker they can't",
+            "Beanbag shotguns are useless against the body, just ignore the warnings and aim for the head",
+            "amogus",
+            "Set your NVGs to white phosphor. Do it",
+            "why tf can't we sprint but we can jump???\n'it ain't realistic' mf i got a full suit of gear and can run fine in it"
         };
 
-        void label2_Click(object sender, EventArgs e)
+        public Form1()
         {
-            return;
+            InitializeComponent();
+            Random random = new();
+            label3.Text = RandomText[random.Next(RandomText.Count)];
+            Application.ApplicationExit += new EventHandler(ExitEventHandler);
+            AppDomain.CurrentDomain.UnhandledException += new UnhandledExceptionEventHandler(ExceptionHandler);
+            richTextBox1.Text = IO.GamePath;
+            checkBox1.Checked = IO.LoadVo;
+            checkBox2.Checked = IO.LoadPak;
+            checkBox3.Checked = IO.LoadFmod;
+            checkBox4.Checked = IO.Loaddx12;
+            checkBox5.Checked = IO.OverrideAll;
         }
 
-        void checkBox5_CheckedChanged(object sender, EventArgs e)
+        async void button1_Click(object sender, EventArgs e)
         {
-            IOScripts.OverrideAll = !IOScripts.OverrideAll;
+            label3.Text = "Transferring files...";
+            await Task.Run(IO.SwitchIO);
         }
 
         async void button2_Click(object sender, EventArgs e)
         {
-            await Task.Run(() => IOScripts.StartDaGame(true));
+            await Task.Run(() => IO.StartDaGame(true));
         }
 
-        void ExitEventHandler(object? sender, EventArgs e) => Task.Run(() => IOScripts.CleanupAsync(true));
-
-        private void button3_Click(object sender, EventArgs e)
+        void button3_Click(object sender, EventArgs e)
         {
             using FolderBrowserDialog dialog = new();
             dialog.InitialDirectory = @"C:\";
@@ -111,6 +74,55 @@ namespace RONML_UI
             {
                 richTextBox1.Text = dialog.SelectedPath;
             }
+        }
+
+        async void button4_Click(object sender, EventArgs e)
+        {
+            await Task.Run(IO.Backup);
+        }
+
+        async void button5_Click(object sender, EventArgs e)
+        {
+            await Task.Run(IO.ImportBackup);
+        }
+
+        void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            IO.LoadVo = !IO.LoadVo;
+        }
+
+        void checkBox2_CheckedChanged(object sender, EventArgs e)
+        {
+            IO.LoadPak = !IO.LoadPak;
+        }
+
+        void checkBox3_CheckedChanged(object sender, EventArgs e)
+        {
+            IO.LoadFmod = !IO.LoadFmod;
+        }
+
+        void checkBox4_CheckedChanged(object sender, EventArgs e)
+        {
+            IO.Loaddx12 = !IO.Loaddx12;
+        }
+
+        void checkBox5_CheckedChanged(object sender, EventArgs e)
+        {
+            IO.OverrideAll = !IO.OverrideAll;
+        }
+
+        void richTextBox1_TextChanged(object sender, EventArgs e)
+        {
+            IO.GamePath = richTextBox1.Text;
+        }
+
+        void ExitEventHandler(object? sender, EventArgs e) => IO.CleanupAsync(true);
+
+        void ExceptionHandler(object? sender, UnhandledExceptionEventArgs args)
+        {
+            label3.ForeColor = Color.DarkRed;
+            Exception e = (Exception)args.ExceptionObject;
+            label3.Text = $"An attempted operation raised an unhandled exception => \n{e.Message}\n{e.StackTrace}";
         }
     }
 }
