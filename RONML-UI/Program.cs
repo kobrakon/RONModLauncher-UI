cdd4b7ea076222061a1833e98cdaa31c6150df44
Zachary Braden <103508025+kobrakon@users.noreply.github.com>
2022-11-10 12:14:23 -0500

    forgor to upload

--- RONML-UI/Program.cs
+++ RONML-UI/Program.cs
@@ -9,10 +9,8 @@ namespace RONML_UI
         ///  The main entry point for the application.
         /// </summary>
         [STAThread]
-        static void Main()
+        public static void Main()
         {
-            // To customize application configuration such as set high DPI settings or default font,
-            // see https://aka.ms/applicationconfiguration.
             ApplicationConfiguration.Initialize();
             Application.Run(Instance);
         }
